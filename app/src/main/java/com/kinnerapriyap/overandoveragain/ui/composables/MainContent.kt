620c2d2a26f2cfbc0466864bafb5eb115b72aeb0
Kinnera Priya Putti <kinnerapriyap@gmail.com>
2024-10-14 18:45:39 +0200

    Initial commit

---
+++ app/src/main/java/com/kinnerapriyap/overandoveragain/ui/composables/MainContent.kt
@@ -0,0 +1,13 @@
+package com.kinnerapriyap.overandoveragain.ui.composables
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainContent(modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello!",
+        modifier = modifier
+    )
+}
