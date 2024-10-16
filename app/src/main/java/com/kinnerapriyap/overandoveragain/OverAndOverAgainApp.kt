00d79a1b66ef8922f98a4b7e9f767ab1647d83b0
Kinnera Priya Putti <kinnerapriyap@gmail.com>
2024-10-16 10:23:21 +0200

    Set sound and vibration to channel

--- app/src/main/java/com/kinnerapriyap/overandoveragain/OverAndOverAgainApp.kt
+++ app/src/main/java/com/kinnerapriyap/overandoveragain/OverAndOverAgainApp.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.media.RingtoneManager
 
 class OverAndOverAgainApp : Application() {
 
@@ -11,9 +12,12 @@ class OverAndOverAgainApp : Application() {
         super.onCreate()
         val name = getString(R.string.channel_name)
         val descriptionText = getString(R.string.channel_description)
-        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
         val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
             description = descriptionText
+            setSound(defaultSoundUri, null)
+            enableVibration(true)
         }
         val notificationManager: NotificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
