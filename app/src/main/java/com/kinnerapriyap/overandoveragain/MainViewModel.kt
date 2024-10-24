2f0ac5870ecbdcffb0025d066ee8d19711d07dbd
Kinnera Priya Putti <kinnerapriyap@gmail.com>
2024-10-24 09:18:41 +0200

    Add time as flow

--- app/src/main/java/com/kinnerapriyap/overandoveragain/MainViewModel.kt
+++ app/src/main/java/com/kinnerapriyap/overandoveragain/MainViewModel.kt
@@ -6,11 +6,17 @@ import com.kinnerapriyap.overandoveragain.alarm.AlarmItem
 import com.kinnerapriyap.overandoveragain.alarm.AlarmRepository
 import com.kinnerapriyap.overandoveragain.alarm.AlarmScheduler
 import com.kinnerapriyap.overandoveragain.alarm.RepeatingAlarmRequest
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
+import java.util.Calendar
 import java.util.UUID
 
 interface MainViewModel {
+    val currentTime: StateFlow<Triple<Int, Int, Int>>
     val repeatingAlarms: Flow<List<AlarmItem>>
     fun scheduleRepeatingAlarm(repeatingAlarmRequest: RepeatingAlarmRequest)
 }
@@ -19,6 +25,23 @@ class DefaultMainViewModel(
     private val repository: AlarmRepository,
     private val alarmScheduler: AlarmScheduler,
 ) : MainViewModel, ViewModel() {
+    private val _currentTime = MutableStateFlow<Triple<Int, Int, Int>>(Triple(0, 0, 0))
+    override val currentTime: StateFlow<Triple<Int, Int, Int>> = _currentTime
+
+    init {
+        viewModelScope.launch {
+            while (isActive) {
+                val calendar = Calendar.getInstance()
+                _currentTime.value =
+                    Triple(
+                        calendar.get(Calendar.HOUR_OF_DAY),
+                        calendar.get(Calendar.MINUTE),
+                        calendar.get(Calendar.SECOND)
+                    )
+                delay(1000)
+            }
+        }
+    }
 
     override val repeatingAlarms: Flow<List<AlarmItem>> = repository.alarms
 
