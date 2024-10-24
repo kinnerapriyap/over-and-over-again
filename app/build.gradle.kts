5822d50c4c8fe3b06850c676a5cf3b34159cb3ca
Kinnera Priya Putti <kinnerapriyap@gmail.com>
2024-10-24 09:18:23 +0200

    Add navigation deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
